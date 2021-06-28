cca10c3b1f5be0585f6083694f4aacacacb3c715
archer <archeryue7@gmail.com>
2021-06-28 21:00:46 +0800

    Update threadpool.c

--- threadpool.c
+++ threadpool.c
@@ -1,10 +1,10 @@
 //coder : archer
 #include "threadpool.h"
 
-extern mythread_pool *pool;
+extern mythread_pool* pool;
 
 void pool_init(int max_thread_num) {
-    pool=(mythread_pool *) malloc(sizeof(mythread_pool));
+    pool=(mythread_pool*) malloc(sizeof(mythread_pool));
 
     pthread_mutex_init(&(pool->queue_lock),NULL);
     pthread_cond_init(&(pool->queue_ready),NULL);
@@ -14,7 +14,7 @@ void pool_init(int max_thread_num) {
     pool->is_shutdown=0;
 
     pool->max_thread_num=max_thread_num;
-    pool->threadid = (pthread_t *) malloc(max_thread_num*sizeof(pthread_t));
+    pool->threadid = (pthread_t*) malloc(max_thread_num*sizeof(pthread_t));
 
     int i=0;
     for (i=0;i<max_thread_num;i++) {
@@ -22,15 +22,15 @@ void pool_init(int max_thread_num) {
     }
 }
 //init the thread pool;
-int pool_add_worker(void *(*process)(void *arg),void *arg) {
-    mythread_worker * newworker=(mythread_worker *) malloc(sizeof(mythread_worker));
+int pool_add_worker(void* (*process)(void* arg),void* arg) {
+    mythread_worker* newworker=(mythread_worker*) malloc(sizeof(mythread_worker));
     newworker->process=process;
     newworker->arg=arg;
     newworker->next=NULL;
 
     pthread_mutex_lock(&(pool->queue_lock));
 
-    mythread_worker *p=pool->queue_head;
+    mythread_worker* p=pool->queue_head;
     if (p!=NULL) {
         while (p->next!=NULL) p=p->next;
         p->next=newworker;
@@ -49,7 +49,7 @@ int pool_add_worker(void *(*process)(void *arg),void *arg) {
     return 0;
 }
 //add work;
-void *thread_routine(void *arg) {
+void* thread_routine(void* arg) {
     arg_t arg_tmp;
     int fd;
 
@@ -76,7 +76,7 @@ void *thread_routine(void *arg) {
         assert(pool->queue_head != NULL);
 
         pool->cur_queue_size--;
-        mythread_worker * myworker=pool->queue_head;
+        mythread_worker* myworker=pool->queue_head;
         pool->queue_head=myworker->next;
 
         pthread_mutex_unlock(&(pool->queue_lock));
@@ -84,7 +84,7 @@ void *thread_routine(void *arg) {
         strcpy(arg_tmp.buf,myworker->arg);
         arg_tmp.fd=fd;
 
-        (*(myworker->process))((void *)&arg_tmp);
+        (*(myworker->process))((void*)&arg_tmp);
         //handle work;
         free(myworker->arg);
         free(myworker);
